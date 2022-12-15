231a33fdc1bc56f723ac4b651c9313270f500b17
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2022-12-14 19:16:10 -0500

    Update Castle_Descent_Terminal_Version.R

--- Castle Descent/R/Castle_Descent_Terminal_Version.R
+++ Castle Descent/R/Castle_Descent_Terminal_Version.R
@@ -16,7 +16,8 @@ fairy_event = function(castle_data,player){
   #Clear object from castle and dataframe
   castle_data$castle[player$movement_coordinate] = '\u2800'
   castle_data$dataframe = castle_data$dataframe[-c(player$castle_dataframe_row),]
-  return(castle_data)
+  fairy_event_output = c(castle_data,player)
+  return(fairy_event_output)
 }
 #Function for genie event
 genie_event = function(castle_data,player){
@@ -29,7 +30,8 @@ genie_event = function(castle_data,player){
   #Clear object from castle and dataframe
   castle_data$castle[player$movement_coordinate] = '\u2800'
   castle_data$dataframe = castle_data$dataframe[-c(player$castle_dataframe_row),]
-  return(castle_data)
+  genie_event_output = c(castle_data,player)
+  return(genie_event_output)
 }
 #Function for monster event
 monster_event = function(castle_data,player){
