ec7eec8321abd32f4d2a3e3ce900022a8cfdc6b0
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2022-12-24 14:34:25 -0500

    Add files via upload

---
+++ Castle Descent/R/R Studio/Castle_Descent_R_Studio.R
@@ -0,0 +1,120 @@
+#Objective is to get to the bottom of the castle and find the exit while avoiding the zombie
+#Can input wasd movement controls freely without having to press enter to advance the script
+
+#Variable used to determine if the player is greeted with the 
+#welcome screen or not
+.iteration <<- 0
+
+castle_descent = function(){
+  
+  #Using castle-create to generate a new random game board
+  castle_data = castle_create()
+  #creating a class for the player
+  player = player_class(coordinate = which(castle_data$castle=='\U1F93A', arr.ind = T),
+                        player_hp = 100,
+                        attack_range=5:10,
+                        #wasd controls will be added to the players current 3d coordinate to 
+                        #obtain a new coordinate corresponding to the direction
+                        movement_dict = list('w' = c(-1,0,0), 'a' = c(0,-1,0), 's' = c(1,0,0),  'd' = c(0,1,0)),
+                        floor = 1, 
+                        encountered_object = '\u2800')
+  
+  zombie = zombie_class(current_coordinate = which(castle_data$castle=='\U1F9DF', arr.ind = T),
+                        initial_coordinate = which(castle_data$castle=='\U1F9DF', arr.ind = T),
+                        movement_dict = list('w' = c(-1,0,0), 'a' = c(0,-1,0), 's' = c(1,0,0),  'd' = c(0,1,0),
+                                             'diag_up_left' = c(-1,-1,0), 'diag_up_right' = c(-1,1,0),
+                                             'diag_down_left' = c(1,-1,0), 'diag_down_right' = c(1,1,0)),
+                        floor = 1)
+  zombie$distance_to_player = zombie$euclidean_distance(zombie$initial_coordinate,player$coordinate)
+  
+  
+  #######################################Setup Complete########################################
+  #if the player decides to play a new game, 
+  #they do not need to be greeted with the welcome screen and objective agin
+  if(.iteration == 0){
+    print('Welcome to Castle Descent!', quote = F)
+    Sys.sleep(1)
+    print('The objective of the game is to descend the bottom of the castle while avoiding the zombie.', quote = F)
+    Sys.sleep(2)
+    print('You will be starting at the top of the castle!', quote = F)
+    Sys.sleep(2)
+  }
+  else{
+    cat(rep("\n", 50))
+    print('New game.', quote = F)
+    Sys.sleep(1)
+  }
+  while(!(player$encountered_object=='\U2395' | player$player_hp <= 0 | zombie$distance_to_player == 0)){
+    #Add multiple spaces so that only a single print out of the game board is on the screen 
+    cat(rep("\n", 50))
+    #Inform player about the current floor they are on
+    print(paste('Floor',player$floor, 'of',length(castle_data$castle)/(nrow(castle_data$castle)*ncol(castle_data$castle))), quote = F)
+    #print current floor the player is on
+    print(castle_data$castle[,,player$floor], quote = F)
+    #player directional inputs
+    read_console_player_movement_action()
+    #Take player current coordinate and add
+    player$movement_coordinate = player$coordinate + player$movement_dict[[player_action]]
+    if(castle_data$castle[player$movement_coordinate] == '\u2800'){
+      player$encountered_object = castle_data$castle[player$movement_coordinate]
+      # Add emoji to new coordinate is the object is empty
+      castle_data$castle[player$coordinate] = '\u2800'
+      #Make previous player coordinate empty
+      player$coordinate = player$movement_coordinate
+      #Movement coordinate now the new player coordinate
+      castle_data$castle[player$coordinate] = '\U1F93A'
+      #Zombie event
+      event_output = zombie$pathfinding(castle_data = castle_data, player = player)
+      castle_data = event_output[1:2]
+      player = event_output[[3]]
+    }
+    else if(castle_data$castle[player$movement_coordinate] =='\U1F6AA'){
+      #Look into dataframe to see what encountered object is supposed to be
+      player$castle_dataframe_row = which(castle_data$dataframe$x == player$movement_coordinate[1] & castle_data$dataframe$y == player$movement_coordinate[2] & castle_data$dataframe$z == player$movement_coordinate[3])
+      number = castle_data$dataframe[player$castle_dataframe_row,'hp']
+      player$encountered_object = castle_data$dataframe[player$castle_dataframe_row,4]
+      
+      if(number > 0){
+        #Switch statement for events
+        switch(player$encountered_object,
+               '\U1F9DA'= {event_output = fairy_event(castle_data = castle_data, player = player)},
+               '\U1F9DE'= {event_output = genie_event(castle_data = castle_data, player = player)},
+               '\U1F479'= {event_output = monster_event(castle_data = castle_data, player = player)},
+               'DS'= {event_output = move_to_next_floor_event(castle_data = castle_data, player = player, zombie = zombie)}
+        )
+        #Collect the outputs from each function
+        castle_data = event_output[1:2]
+        player = event_output[[3]]
+      }
+      else if(number == 0){
+        cat(rep("\n", 50))
+        if(castle_data$dataframe[player$castle_dataframe_row,'object'] == 'AS'){
+          print('You already came from upstairs.', quote = F)
+        }
+        else{
+          print('There is nothing behind this door.', quote = F)
+        }
+        print(castle_data$castle[,,player$floor], quote = F)
+        Sys.sleep(2)
+        
+      }
+      
+    }
+  }
+  #if statement to continue or quit when they die
+  if(player$player_hp <= 0 | zombie$distance_to_player == 0){
+    .iteration <<- .iteration + 1
+    read_console_try_again_action()
+  }
+  else{
+    #else statement o replay if they win
+    cat(rep("\n", 50))
+    print('You found the exit!', quote = F)
+    castle_data$castle[player$movement_coordinate] = player$encountered_object
+    print(castle_data$castle[,,player$floor], quote = F)
+    .iteration <<- .iteration + 1
+    read_console_try_again_action()
+  }
+}
+
+castle_descent()
---
+++ Castle Descent/R/R Studio/Classes.R
@@ -0,0 +1,86 @@
+#Script for zombie and player class
+#Zombie class contains the necessary attributes to keep track of zombie coordinates
+#and methods to allow zombie to move
+zombie_class = setRefClass('zombie_info', fields = list(movement_dict = 'list',
+                                                        initial_coordinate = 'matrix',
+                                                        current_coordinate = 'matrix',
+                                                        distance_to_player= 'numeric',
+                                                        floor = 'numeric'),
+                           methods = list(
+                             euclidean_distance = function(a,b){
+                               c_squared = sqrt(sum((a - b)^2))
+                               return(c_squared)
+                             },
+                             pathfinding = function(castle_data,player){
+                               move_or_dont = sample(c(0,1),size = 1, prob = c(0.70,0.30))
+                               if(move_or_dont == 1){
+                                 #Using the current coordinate, get list of eight possible coordinates that allows movement in eight directions
+                                 movement_vector = c()
+                                 for(movement in movement_dict){
+                                   possible_coordinate = current_coordinate + movement
+                                   #If coordinate is empty or contains the player, it is a possible coordinate to move to
+                                   if(castle_data$castle[possible_coordinate] == '\u2800'| castle_data$castle[possible_coordinate] == '\U1F93A'){
+                                     movement_vector = c(movement_vector, list(possible_coordinate))
+                                   }                            
+                                 }
+                                 #Calculate total cost = distance cost + manhattan distance from player
+                                 #Diagonals will cost more farthest from the initial coordinate will cost more but ones closest to the initial 
+                                 #position will cost less
+                                 movement_cost = c()
+                                 for(possible_coordinate in movement_vector){
+                                   #Coordinates with lower manhattan distance from initial will have lower cost
+                                   cost = sum(abs(initial_coordinate - current_coordinate))
+                                   c_squared = euclidean_distance(possible_coordinate,player$coordinate)
+                                   movement_cost = c(movement_cost, c_squared + cost)
+                                 }
+                                 
+                                 #Erase zombie from old location and add to new location
+                                 castle_data$castle[which(castle_data$castle=='\U1F9DF', arr.ind = T)] = '\u2800'
+                                 current_coordinate <<-  movement_vector[[which(movement_cost == min(movement_cost))[1]]]
+                                 castle_data$castle[current_coordinate] = '\U1F9DF'
+                                 #Calculate new distance from zombie to player
+                                 distance_to_player <<- euclidean_distance(current_coordinate,player$coordinate)
+                                 if(distance_to_player == 0){
+                                   cat(rep("\n", 50))
+                                   print(castle_data$castle[,,player$floor], quote = F)
+                                   print('You were eaten by the zombie' , quote = F)
+                                 }
+                                 pathfinder_output = c(castle_data,player)
+                                 return(pathfinder_output)
+                               }
+                               else{
+                                 pathfinder_output = c(castle_data,player)
+                                 return(pathfinder_output)
+                               }
+                               
+                             },
+                             #Allow zombie to move ot new floor with player
+                             move_to_new_floor_event = function(castle_data,player){
+                               #Find the coordinate that allows the zombie to be at the greatest Euclidean distance from the
+                               #player
+                               movable_spaces = which(castle_data$castle[,,player$floor]=='\u2800', arr.ind = T)
+                               c_squared = c()
+                               for(row in 1:nrow(movable_spaces)){
+                                 c_squared = euclidean_distance(movable_spaces[row,],player$coordinate[1:2])
+                               }
+                               coord = movable_spaces[which(c_squared == max(c_squared)),]
+                               #Erase old zombie location
+                               castle_data$castle[which(castle_data$castle=='\U1F9DF', arr.ind = T)] = '\u2800'
+                               #Add new zombie location and update initial coordinate,current coordinate, and distance
+                               castle_data$castle[,,player$floor][coord[1],coord[2]] = '\U1F9DF'
+                               initial_coordinate <<- which(castle_data$castle=='\U1F9DF', arr.ind = T)
+                               current_coordinate <<- which(castle_data$castle=='\U1F9DF', arr.ind = T)
+                               distance_to_player <<- euclidean_distance(current_coordinate,player$coordinate)
+                               move_to_new_floor_event_output = c(castle_data,player)
+                               return(move_to_new_floor_event_output)
+                             }))
+
+#Player class keeps importint information about player such as health and current posisiton
+player_class =  setRefClass('player_info', fields = list(player_hp='numeric', coordinate='matrix',
+                                                         movement_dict = 'list',
+                                                         encountered_object='character',
+                                                         movement_coordinate = 'matrix',
+                                                         attack_range='numeric', 
+                                                         attack_power = 'numeric',
+                                                         floor = 'numeric',
+                                                         castle_dataframe_row = 'numeric'))
